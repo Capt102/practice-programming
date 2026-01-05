bb6c7cf6688ad9ef7ef3b3592de2ed2d6928f227
fayas <kennyjake407@gmail.com>
2026-01-05 10:36:12 +0530

    first number

---
+++ first.py
@@ -0,0 +1,7 @@
+"""
+complete the function print number take an integer input from user and print it on screen
+"""
+class Solution:
+    def printnumber(self):
+        number=int(input())
+        print(number)
